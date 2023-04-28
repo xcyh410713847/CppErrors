6f7400e630807ec8e3c397086655aaf16f78d1f3
shyfan <410713847@qq.com>
2023-04-28 16:11:37 +0800

    错误信息和堆栈信息分开

--- CppErrors/CppErrors.h
+++ CppErrors/CppErrors.h
@@ -14,10 +14,13 @@
 
 using namespace std;
 
+string emptyTrace = "";
+
 template <typename T>
 class CppErrors
 {
 	vector<T> errors;
+	vector<string> errorTrace;
 
 public:
 	CppErrors(){};
@@ -27,9 +30,10 @@ public:
 	}
 
 	// 添加错误
-	void AddError(const T &error)
+	void AddError(const T &error, string trace)
 	{
 		errors.push_back(error);
+		errorTrace.push_back(trace);
 	}
 
 	// 获取错误列表
@@ -64,11 +68,15 @@ public:
 	}
 };
 
-#define CPP_ERRORS_WRAP(cppErrors, error)                                                  \
-	{                                                                                      \
-		stringstream ss;                                                                   \
-		ss << "[" << __FILE__ << ":" << __LINE__ << "(" << __FUNCTION__ << ")] " << error; \
-		cppErrors.AddError(ss.str());                                                      \
+// 不带堆栈的错误
+#define CPP_ERRORS_WRAP(cppErrors, error) cppErrors.AddError(error, emptyTrace)
+
+// 带堆栈的错误
+#define CPP_ERRORS_WRAP_TRACE(cppErrors, error, trace)                            \
+	{                                                                             \
+		stringstream ss;                                                          \
+		ss << "[" << __FILE__ << ":" << __LINE__ << "(" << __FUNCTION__ << ")] "; \
+		cppErrors.AddError(error, ss.str());                                      \
 	}
 
 #endif // __CPP_ERRORS_H__
